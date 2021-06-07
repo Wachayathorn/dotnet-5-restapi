7f86e08526cc529edc977675d8e58d9f2019ff89
Wachayathorn Singsena <wachayathorn.s@rvconnex.com>
2021-06-07 15:16:10 +0700

    feat: add dockerfile

--- dotnet-5/Program.cs
+++ dotnet-5/Program.cs
@@ -11,6 +11,7 @@ namespace dotnet_5
 {
     public class Program
     {
+        [STAThread]
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
