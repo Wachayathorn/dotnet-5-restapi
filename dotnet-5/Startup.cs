0abedd850d4da3bcd494792440b1a222f8eef801
Wachayathorn Singsena <wachayathorn.s@rvconnex.com>
2021-06-08 16:29:40 +0700

    feat: add dockerfile and docker-compose

--- dotnet-5/Startup.cs
+++ dotnet-5/Startup.cs
@@ -43,11 +43,11 @@ namespace dotnet_5
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                    app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API Docs v1"));
+                app.UseHttpsRedirection();
             }
 
-            app.UseHttpsRedirection();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API Docs v1"));
 
             app.UseRouting();
 
